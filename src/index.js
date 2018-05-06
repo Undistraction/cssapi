9fbad897daf74e90075d7ee0ca8cb49f07c6b487
Pedr Browne <pedr.browne@gmail.com>
2018-05-06 13:26:38 +0100

    feat(scope): alias to ’s’

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import cssapi from './cssapi'
 export default cssapi
 
 export { scope } from './utils/scope'
+export { scope as s } from './utils/scope'
 export { default as api } from './themeHelpers/api'
