4b2c92bc51c7efa7561dcd84c962f3d0e58ef42e
Pedr Browne <pedr.browne@gmail.com>
2018-05-06 13:26:58 +0100

    refactor: remove stray logging

--- src/themeHelpers/api.js
+++ src/themeHelpers/api.js
@@ -2,15 +2,13 @@ import { apply, flip, pipe, prop, unless } from 'ramda'
 import { isFunction, isPlainObj } from 'ramda-adjunct'
 import { noAPIOnThemeError, noThemeObjectError, throwAPIError } from '../errors'
 
-const api = declarations => props => {
-  console.log(`VV`, props)
-  return pipe(
+const api = declarations =>
+  pipe(
     prop(`theme`),
     unless(isPlainObj, value => throwAPIError(noThemeObjectError(value))),
     prop(`api`),
     unless(isFunction, value => throwAPIError(noAPIOnThemeError(value))),
     flip(apply)([declarations])
-  )(props)
-}
+  )
 
 export default api
